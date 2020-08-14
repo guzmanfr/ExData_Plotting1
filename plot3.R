0e7c6a315d50df8605a0a5b3fef789a3bc912eec
Francisco Guzman <63519078+guzmanfr@users.noreply.github.com>
2020-08-14 10:11:50 -0500

    Update to plot3.R

--- plot3.R
+++ plot3.R
@@ -27,9 +27,9 @@ with(data1, {
  lines(Sub_metering_2~Datetime,col = 'Red')
  lines(Sub_metering_3~Datetime,col = 'Blue')
 })
-legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-# Annotating the graph:
+# Annotating the graph
+legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 title(main="Energy sub-metering")
 
 ## Writes the PNG file and closes the device
